b5f4c40a00f2239661a6b544d3bc099cf92238fb
Douglas Meehan <dmeehan@gmail.com>
2020-07-13 15:52:58 -0400

    fixed manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Douglas Meehan`,
+        short_name: `dm`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#63C9D7`,
+        theme_color: `#63C9D7`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/contour_bg.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
