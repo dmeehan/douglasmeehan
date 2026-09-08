610111788af372572751c45368e81388b5959128
Douglas Meehan <dmeehan@gmail.com>
2026-09-08 10:49:28 -0400

    Refactor subtitle for improved readability
    Revised the subtitle for clarity and conciseness.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,9 @@ export default ({ data }) => (
     <div className="page">
   <h1><span>Hello!&nbsp;</span><span>I'm Douglas Meehan.</span></h1>
     <h2 className="page-subtitle">
-      I'm a <strong>product design leader focused on complex systems.</strong>
-      I lead product design at Citrine Informatics, where I design AI software
-      used by scientists and engineers. Throughout my career—from digital
-      products to urban design and back to software—I've been drawn to problems
-      where people need to understand complicated systems in order to make good decisions.
+      I'm a <strong>product design leader focused on complex systems.</strong> I lead product design at Citrine Informatics, where I design AI software
+      used by scientists and engineers. Throughout my career—from digital products to urban design and back to software —
+      I've been drawn to problems where people need to understand how a system works in order to make informed decisions.
     </h2>
       <div className="page-projects">
         {renderProjects(data.allMarkdownRemark.edges)}
