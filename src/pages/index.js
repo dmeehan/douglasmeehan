cbeea17833c6b37966d8cab7e94164f304cbd17d
Douglas Meehan <dmeehan@gmail.com>
2026-09-08 10:51:33 -0400

    Revise subtitle for better readability
    Updated text to improve clarity and flow.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ export default ({ data }) => (
   <h1><span>Hello!&nbsp;</span><span>I'm Douglas Meehan.</span></h1>
     <h2 className="page-subtitle">
       I'm a <strong>product design leader focused on complex systems.</strong> I lead product design at Citrine Informatics, where I design AI software
-      used by scientists and engineers. Throughout my career — from digital products to urban design and back to software —
-      I've been drawn to problems where people need to understand how a system works in order to make informed decisions.
+      used by scientists and engineers. Throughout my career—from digital products to urban design and back to software— I've been drawn to problems where 
+      people need to navigate complexity in order to make informed decisions.
     </h2>
       <div className="page-projects">
         {renderProjects(data.allMarkdownRemark.edges)}
